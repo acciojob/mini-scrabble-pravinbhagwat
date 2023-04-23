3b04af6a653813624759ff42ea86cbf6fc99db16
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-04-23 19:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+
+const textbox = document.getElementById('evaluatedText');
+const letterCount = document.getElementById('letterCount');
+
+
+textbox.addEventListener('input', function() {
+
+  const text = this.value;
+
+  const count = text.length;
+
+  letterCount.textContent = count;
+});
